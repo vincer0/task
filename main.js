6e940314e07efcb61a12b8b2b8de9c720864f7b7
Vincer0 <pasierb.adam@gmail.com>
2020-07-10 13:22:06 +0200

    refactor variable current

--- main.js
+++ main.js
@@ -10,11 +10,10 @@ const list = function(numerOfElements) {
 
 const listTracksRandomly = function(list) {
     let randomList = [];
-    let current = null;
     let listLength = list.length
 
     for(let i = 0; i < listLength; i++) {
-        current = list[Math.floor(Math.random() * list.length)];
+        let current = list[Math.floor(Math.random() * list.length)];
         randomList[i] = current
         let idx = list.indexOf(current)
         list.splice(idx, 1)
